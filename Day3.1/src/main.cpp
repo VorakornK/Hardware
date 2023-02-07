3c16212d2add093edf7ef75f31720b706b95ea91
Wynnter <sittipol1012@gmail.com>
2023-02-07 11:11:23 +0700

    solution exercise1

--- Day3.1/src/main.cpp
+++ Day3.1/src/main.cpp
@@ -1,36 +1,56 @@
 #include <Arduino.h>
+#include <bounce2.h>
+#define BUTTON 27
+#define GREEN 33
+#define RED 26
+#define YELLOW 25
+#define LDR 32
 
-#define BLUE 5
-#define RED 12
+Bounce debouncer = Bounce();
+
 TaskHandle_t TaskA = NULL;
 TaskHandle_t TaskB = NULL;
 
-void Blink_LED(void *param){
-    while(1){
-        digitalWrite(RED, 1);
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        digitalWrite(RED, 0);
-        vTaskDelay(500/portTICK_PERIOD_MS);
+int count = 0;
+
+void LED_bright(void *param){
+  while(1){
+  for (int i = 0; i < 255; i++) {
+    ledcWrite(0, i);
+    vTaskDelay(5/portTICK_PERIOD_MS);
+  }
+  for (int i = 255; i > 0; i--) {
+    ledcWrite(0, i);
+    vTaskDelay(5/portTICK_PERIOD_MS);
+  }
+  vTaskDelay(1000/portTICK_PERIOD_MS);
+}
+}
+
+void count_button(void *param){
+  while (1) {
+    debouncer.update();
+    if (debouncer.fell()) {
+      count++;
+      Serial.println(count);
     }
+  }
 }
+void setup()
+{
+  Serial.begin(115200);
+  ledcSetup(0, 5000, 8);
+  ledcAttachPin(GREEN, 0);
+  ledcAttachPin(YELLOW, 0);
+  ledcAttachPin(RED, 0);
+  pinMode(BUTTON, INPUT_PULLUP);
+  debouncer.attach(BUTTON);
+  debouncer.interval(25);
 
-void Lit_LED(void *param){
-    while(1){
-        digitalWrite(BLUE, 0);
-        vTaskDelay(200/portTICK_PERIOD_MS);
-        digitalWrite(BLUE, 1);
-        vTaskDelay(200/portTICK_PERIOD_MS);
-    }
+  xTaskCreatePinnedToCore(LED_bright, "LED_bright", 1000, NULL, 1, &TaskA, 0);
+  xTaskCreatePinnedToCore(count_button, "count_button", 1000, NULL, 1, &TaskB, 1);
 }
-
-void setup(){
-    pinMode(BLUE, OUTPUT);
-    pinMode(RED, OUTPUT);
-
-    xTaskCreatePinnedToCore(Lit_LED, "Builtin_LED", 1000, NULL, 1, &TaskA, 0);
-    xTaskCreatePinnedToCore(Blink_LED, "Blink_LED", 1000, NULL, 1, &TaskB, 1);
-}
-
-void loop(){
+void loop()
+{
 }
 
