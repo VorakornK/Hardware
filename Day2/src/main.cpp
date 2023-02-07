ae1dd939fe57cdb4be5191c071b517868ca577a0
VorakornK <vorakorn.ko@ku.th>
2023-02-07 13:22:15 +0700

    Day2

--- Day2/src/main.cpp
+++ Day2/src/main.cpp
@@ -1,108 +1,83 @@
+// If you want to run in WOKWi
+// change pin and wifi
 #include <Arduino.h>
-#include <ArduinoJson.h>
 #include <HTTPClient.h>
-#include <string>
+#include <ArduinoJson.h>
+#include <Bounce2.h>
+#include "traffic.h"
 
+#define BUTTON 27
+#define GREEN 33
+#define RED 26
+#define YELLOW 25
+#define LDR 32
 
-// 0 = Green
-// 1 = Yellow
-// 2 = Red
+const char *ssid = "Qwerty";
+const char *password = "12345678";
 
-const char *ssid = "ZodiaX";
-const char *password = "poom1234";
+int stage = 0;
+int lumine = 0;
+Bounce debouncer = Bounce();
 
-const String baseUrl = "https://exceed-hardware-stamp465.koyeb.app";
-const String GroupNum = "1";
-const String AGurl = "https://exceed-hardware-stamp465.koyeb.app/all_traffic";
+void Connect_Wifi();
 
-const String GroupCode = "317cwk";
-
-
-const String point = "1";
-const int nearby_1 = 2;
-const int nearby_2 = 15;
-
-
-void GET_traffic() {
-  Serial.println("Start!!!");
-  DynamicJsonDocument doc(65536);
-  HTTPClient http;
-  http.begin(AGurl);
-
-  Serial.println("Nearby traffic");
-  int httpResponseCode = http.GET();
-  if (httpResponseCode == 200)
-  {
-    String payload = http.getString();
-    deserializeJson(doc, payload);
-    JsonArray all_traffic = doc["all_traffic"].as<JsonArray>();
-
-    Serial.println("Point: " + doc["all_traffic"][0]["point"].as<String>());
-    Serial.println("Traffic: " + doc["all_traffic"][0]["traffic"].as<String>());
-
-    Serial.println("Point: " + doc["all_traffic"][nearby_1 - 1]["point"].as<String>());
-    Serial.println("Traffic: " + doc["all_traffic"][1][nearby_1 - 1].as<String>());
-
-    Serial.println("Point: " + doc["all_traffic"][nearby_2 - 1]["point"].as<String>());
-    Serial.println("Traffic: " + doc["all_traffic"][nearby_2 - 1]["traffic"].as<String>());
-  }
-  else
-  {
-    Serial.print("Error ");
-    Serial.println(httpResponseCode);
-  }
-
-  Serial.println("----------------------------------");
+void setup() {
+    Serial.begin(115200);
+    pinMode(BUTTON, INPUT_PULLUP);
+    debouncer.attach(BUTTON);
+    debouncer.interval(5);
+    pinMode(GREEN, OUTPUT);
+    pinMode(YELLOW, OUTPUT);
+    pinMode(RED, OUTPUT);
+    Connect_Wifi();
+    digitalWrite(GREEN, HIGH);
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(RED, LOW);
+    POST_traffic(stage);   
+    GET_traffic();
 }
 
-void POST_traffic(String led)
+
+void loop(){
+    debouncer.update();
+    if (debouncer.fell() && stage == 0) { //if button pressed and stage is green
+      stage = 1;
+      digitalWrite(GREEN, LOW);
+      digitalWrite(YELLOW, HIGH);
+      digitalWrite(RED, LOW);
+      POST_traffic(1);   
+      delay(8000);
+      digitalWrite(GREEN, LOW);
+      digitalWrite(YELLOW, LOW);
+      digitalWrite(RED, HIGH);
+      POST_traffic(2);
+      GET_traffic();
+      delay(5000); //wait 5 seconds
+      lumine = map(analogRead(LDR),1500,3500,0,255);
+      while (lumine >= 100) {
+        lumine = map(analogRead(LDR),1500,3500,0,255);
+      }
+      digitalWrite(GREEN, HIGH);
+      digitalWrite(YELLOW, LOW);
+      digitalWrite(RED, LOW);
+      POST_traffic(0);   
+      GET_traffic();
+      stage = 0;
+    }
+    
+    //Serial.println(stage); //print stage
+}
+
+void Connect_Wifi()
 {
-  const String url = baseUrl + "/my_traffic?point=" + point;
-  String json;
-  HTTPClient http;
-  http.begin(url);
-  http.addHeader("Content-Type", "application/json");
-
-  DynamicJsonDocument doc(2048);
-  doc["code"] = "317cwk";
-  doc["traffic"] = led;
-  serializeJson(doc, json);
-
-  Serial.println("POST " + led);
-  int httpResponseCode = http.POST(json);
-  if (httpResponseCode == 200)
-  {
-    Serial.print("Done");
-    Serial.println();
-  }
-  else
-  {
-    Serial.print("Error ");
-    Serial.println(httpResponseCode);
-  }
-
-  Serial.println("----------------------------------");
-}
-
-
-void Connect_Wifi() {
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.print("OK! IP=");
   Serial.println(WiFi.localIP());
-}
-
-void setup() {
-  Serial.begin(115200);
-  Connect_Wifi();
-  POST_traffic("Yellow");
-  GET_traffic();
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("----------------------------------");
 }
---
+++ Day2/src/traffic.h
@@ -0,0 +1,80 @@
+#include <Arduino.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <Bounce2.h>
+
+//0 1 2
+//G Y R
+
+const String baseUrl = "https://api-hardware.onrender.com";
+//const String baseUrl = "https://exceed-hardware-stamp465.koyeb.app";
+//https://api-hardware.onrender.com
+const String AGurl = "https://api-hardware.onrender.com/all_traffic";
+
+const String point = "1";
+const int nearby_1 = 2;
+const int nearby_2 = 15;
+const String Color[3] = {"Green", "Yellow", "Red"};
+
+void GET_traffic() {
+  Serial.println("Start!!!");
+  DynamicJsonDocument doc(65536);
+  HTTPClient http;
+  http.begin(AGurl);
+  http.setTimeout(10000);
+  
+  Serial.println("Nearby traffic");
+  int httpResponseCode = http.GET();
+  if (httpResponseCode == 200)
+  {
+    String payload = http.getString();
+    deserializeJson(doc, payload);
+    JsonArray all_traffic = doc["all_traffic"].as<JsonArray>();
+
+    Serial.println("Point: " + doc["all_traffic"][0]["point"].as<String>());
+    Serial.println("Traffic: " + doc["all_traffic"][0]["traffic"].as<String>());
+
+    Serial.println("Point: " + doc["all_traffic"][nearby_1 - 1]["point"].as<String>());
+    Serial.println("Traffic: " + doc["all_traffic"][1][nearby_1 - 1].as<String>());
+
+    Serial.println("Point: " + doc["all_traffic"][nearby_2 - 1]["point"].as<String>());
+    Serial.println("Traffic: " + doc["all_traffic"][nearby_2 - 1]["traffic"].as<String>());
+  }
+  else
+  {
+    Serial.print("Error ");
+    Serial.println(httpResponseCode);
+  }
+
+  Serial.println("----------------------------------");
+}
+
+void POST_traffic(int led)
+{
+  const String url = baseUrl + "/my_traffic?point=" + point;
+  String json;
+  HTTPClient http;
+  http.begin(url);
+  http.setTimeout(10000);
+  http.addHeader("Content-Type", "application/json");
+
+  DynamicJsonDocument doc(2048);
+  doc["code"] = "317cwk";
+  doc["traffic"] = Color[led];
+  serializeJson(doc, json);
+
+  Serial.println("POST " + Color[led]);
+  int httpResponseCode = http.POST(json);
+  if (httpResponseCode == 200)
+  {
+    Serial.print("Done");
+    Serial.println();
+  }
+  else
+  {
+    Serial.print("Error ");
+    Serial.println(httpResponseCode);
+  }
+
+  Serial.println("----------------------------------");
+}
